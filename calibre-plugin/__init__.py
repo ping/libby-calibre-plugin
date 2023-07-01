039381b31e05f0038b71ed709ff2b8cfa633310b
ping <ping@users.noreply.github.com>
2023-07-02 07:57:16 +0800

    Bump version to 0.1.2

--- calibre-plugin/__init__.py
+++ calibre-plugin/__init__.py
@@ -16,7 +16,7 @@ from calibre.customize import InterfaceActionBase
 
 load_translations()
 
-__version__ = (0, 1, 1)
+__version__ = (0, 1, 2)
 PLUGIN_NAME = "overdrive_libby"
 PLUGIN_ICON = "images/plugin.png"
 

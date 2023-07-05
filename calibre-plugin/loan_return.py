d86f704631b790eb03c00b2a44ec14a453afb2ae
ping <ping@users.noreply.github.com>
2023-07-05 12:14:05 +0800

    Log loan return success

--- calibre-plugin/loan_return.py
+++ calibre-plugin/loan_return.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 from .libby import LibbyClient
+from .model import get_loan_title
 
 
 class LibbyLoanReturn:
@@ -13,5 +14,7 @@ class LibbyLoanReturn:
         abort=None,
         notifications=None,
     ):
+        logger = log
         notifications.put((0.5, "Returning"))
         libby_client.return_loan(loan)
+        logger.info("Returned %s successfully." % get_loan_title(loan))

0a46071fdcad1ab2c6f4625de23de08f1d8bc368
ping <ping@users.noreply.github.com>
2023-07-06 14:24:56 +0800

    Update translate helper script

--- translate.sh
+++ translate.sh
@@ -12,15 +12,16 @@ xgettext -L Python \
   --package-version="$version" \
   --msgid-bugs-address='https://github.com/ping/libby-calibre-plugin/' \
   --copyright-holder='ping <http://github.com/ping>' \
-  -o calibre-plugin/translations/default.pot calibre-plugin/*.py
+  -o calibre-plugin/translations/default.pot calibre-plugin/*.py calibre-plugin/**/*.py
 
 # https://www.gnu.org/software/gettext/manual/gettext.html#msgmerge-Invocation
 for f in calibre-plugin/translations/*.po
 do
   echo "Updating ${f} from default.pot"
-  msgmerge --update "${f}" "calibre-plugin/translations/default.pot"
+  msgmerge --no-fuzzy-matching --update "${f}" "calibre-plugin/translations/default.pot"
   sed -i'' -e "s/Project-Id-Version: .*\\\n/Project-Id-Version: ${package_name} ${version}\\\n/" $f
   rm -f "${f}-e"
+  rm -f "${f}~"
   echo "Building ${f} into ${f%\.po}.mo"
   msgfmt -o "${f%\.po}.mo" "${f%\.po}"
 done
